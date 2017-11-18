63a21a2b3e79d1e652721b000f93759825443add
Christoph Dorn <christoph@christophdorn.com>
2017-11-17 23:20:14 -0800

    wip

--- examples/02-RunBashOriginCall/impl/server.js
+++ examples/02-RunBashOriginCall/impl/server.js
@@ -1,19 +1,18 @@
 
-const codeblock = require("codeblock");
-const http = require("http");
+const LIB = require("bash.origin.workspace").forPackage(__dirname + "/../../..").LIB;
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT;
 
 const config = JSON.parse(process.argv[2]);
 
-const responder = codeblock.run(config.responder, {}, {
+const responder = LIB.CODEBLOCK.run(config.responder, {}, {
     sandbox: {
         console: console
     }
 });
 
-const server = http.createServer(responder);
+const server = LIB.HTTP.createServer(responder);
 
 server.listen(port, hostname, () => {
     console.log("[node] Server running at http://" + hostname + ":" + port + "/");
